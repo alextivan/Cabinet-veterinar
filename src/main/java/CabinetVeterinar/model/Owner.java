d688e957a7d4fd9ce600d4826fc65aa998947e31
Alex <alextivan@gmail.com>
2021-05-22 11:32:51 +0300

    Owner Versiune1 Dary

--- src/main/java/CabinetVeterinar/model/Owner.java
+++ src/main/java/CabinetVeterinar/model/Owner.java
@@ -1,4 +1,58 @@
 package CabinetVeterinar.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "owners")
 public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "id_person")
+    private Integer idPerson;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToOne(mappedBy = "owner") // person este numele fieldului din Doctor
+    private Doctor doctor;
+
+    public Owner(Integer idPerson, String address) {
+
+        this.idPerson = idPerson;
+        this.address = address;
+    }
+
+    public Owner() {
+
+    }
+
+    public Integer getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(Integer idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "idPerson" + idPerson +
+                '}';
+
+    }
 }
